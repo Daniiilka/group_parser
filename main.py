2b7e088eabf41492e0797b72367c0649a384e2a0
Daniiilka <romanovdaniil71@gmail.com>
2022-06-29 18:34:27 +0300

    bag with time fixed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import argparse
-import datetime
 import json
 
 import pymongo
@@ -151,9 +150,7 @@ if __name__ == "__main__":
         if args.storage == "mongo":
             mycol.insert_one(mydict)
         else:
-            with open(
-                f"sample{datetime.datetime.now()}.json", "a+", encoding="utf-8"
-            ) as outfile:
+            with open("sample.json", "a+", encoding="utf-8") as outfile:
                 json.dump(mydict, outfile, ensure_ascii=False)
 
         # setting up waiting for a changes in the URL link
